e276b6097aaa7387ed3f494befbc8a590ea0183e
Hugo Olavarria <olavarriahugo@hotmail.com>
2023-06-14 07:54:58 -0300

    en proceso

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,12 +5,18 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if (x === y)
+   return true;
+   else return false
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if (ntr1.leng===ntr2.leng)
+   return true;
+   else return false;
 }
 
 function menosQueNoventa(num) {
